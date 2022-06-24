7db95bacc12bc9763b612409e2f2632acdb882fc
Manon <81025586+ManonLef@users.noreply.github.com>
2022-06-24 23:32:31 +0200

    add for loop to generate squares in grid

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // create function that decides div width/height with 640px/grid size.
+
+const grid = document.querySelector('.grid');
+
+
+function gridGenerator(gridWidth) {
+    for (let i = 0; i < (gridWidth * gridWidth); i++) {
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        grid.appendChild(squares);
+    }
+}
+
+gridGenerator(16);
