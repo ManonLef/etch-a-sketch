14587fc6b74ad5b3d43c46125eed717e73b9a901
Manon <81025586+ManonLef@users.noreply.github.com>
2022-06-25 14:47:37 +0200

    add functionality to calculate pixel sides

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // create function that decides div width/height with 640px/grid size.
+// squares size = 640px / # of squares
+
+let pixelAmount = prompt("How many squares wide would you like the grid to be?");
+let sides = 640/pixelAmount + "px";
 
 const grid = document.querySelector('.grid');
 
@@ -6,13 +10,18 @@ const grid = document.querySelector('.grid');
 function gridGenerator(gridWidth) {
     for (let i = 0; i < (gridWidth * gridWidth); i++) {
         const squares = document.createElement('div');
-        squares.classList.add('squares');
+        squares.classList.add('squares');     
+         //add css to change width and height
+         squares.style.width = sides;
+         squares.style.height = sides;
+         console.log(sides);
+         //
         grid.appendChild(squares);
     }
     testColor();
 }
 
-gridGenerator(16);
+gridGenerator(pixelAmount);
 
 function testColor() {
     const squares = document.querySelectorAll('.squares');
