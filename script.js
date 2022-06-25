f360b8ada6928f162a2f99d5028e14ba4a84b391
Manon <81025586+ManonLef@users.noreply.github.com>
2022-06-25 17:19:03 +0200

    add button click eventlistn and make it generate grid

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+const gridButton = document.querySelector('.gridButton');
+gridButton.addEventListener('click', () => {
+    let pixelAmount = prompt("How many squares wide would you like the grid to be?");
+    gridGenerator(pixelAmount);
+});
 
 
-let pixelAmount = prompt("How many squares wide would you like the grid to be?");
-let sides = 640/pixelAmount + "px";
 
 const grid = document.querySelector('.grid');
-
+const gridPrompt = document.querySelector('.gridPrompt');
 
 function gridGenerator(gridWidth) {
     for (let i = 0; i < (gridWidth * gridWidth); i++) {
+        let sides = 640/gridWidth + "px";
         const squares = document.createElement('div');
         squares.classList.add('squares');     
          //css to change width and height
@@ -30,6 +34,6 @@ function testColor() {
     });
 }
 
-gridGenerator(pixelAmount);
+
 
 
